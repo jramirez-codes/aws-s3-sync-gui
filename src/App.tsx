89267114a7b4600dc0bf2c450e687e5569face62
jordan <jordan.s.ramirez@gmail.com>
2024-11-01 06:30:55 -0400

    delete a card

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,30 @@ import { Topbar } from "./components/topbar/topbar";
 import React from "react";
 import { S3Link } from "./types/s3Link";
 import { load } from "@tauri-apps/plugin-store";
+
 function App() {
   const [s3Links, setS3Links] = React.useState<S3Link[]>([])
+  const [selectedS3Link, setSelectedS3Link] = React.useState<S3Link | null>(null)
+
+  async function syncSingleS3Link(idx: number) {
+    console.log(s3Links[idx])
+  }
+
+  async function handleDeleteS3Link(idx: number) {
+    // Remove Link From State
+    let reducedS3Links = s3Links.filter((_,currIdx)=> currIdx !== idx)
+    setS3Links(_=>reducedS3Links)
+    
+    // Update State Store
+    const store = await load('store.json', { autoSave: false });
+    await store.set('s3Links', reducedS3Links)
+    await store.save()
+  }
+
+  async function handleOpenInfo(idx: number) {
+    setSelectedS3Link(s3Links[idx])
+  }
+
 
   // Fetch Links
   React.useEffect(() => {
@@ -29,10 +51,21 @@ function App() {
     <>
       <div className="h-[100vh] w-[100vw] p-2">
         <Topbar setS3Links={setS3Links} />
-        <div className="grid grid-cols-4 gap-4">
-          {s3Links.map((e) => {
+        <div className="grid grid-cols-4 gap-4 items-stretch">
+          {s3Links.map((e, idx) => {
             return (
-              <FolderCard s3Link={e}/>
+              <FolderCard s3Link={e} idx={idx} key={idx+e.title}
+                onSyncData={(idx: number) => {
+                  syncSingleS3Link(idx)
+                }}
+                onDelete={(idx: number) => {
+                  // console.log("DELETE", idx)
+                  handleDeleteS3Link(idx)
+                }}
+                onOpenInfo={(idx: number) => {
+                  handleOpenInfo(idx)
+                }}
+              />
             )
           })}
         </div>
--- src/components/folderCard/folderCard.tsx
+++ src/components/folderCard/folderCard.tsx
@@ -8,21 +8,34 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { S3Link } from "../../types/s3Link";
+import { TrashIcon } from "@radix-ui/react-icons";
+import React from "react";
+export function FolderCard(props: {
+  s3Link: S3Link,
+  idx: number,
+  onSyncData: Function,
+  onDelete: Function,
+  onOpenInfo: Function
+}) {
+  const [isHovering, setIsHovering] = React.useState<boolean>(false)
 
-export function FolderCard(props: {s3Link: S3Link}) {
-  console.log(props.s3Link)
   return (
-    <div className="inline">
-      <Card>
+    <div className="inline h-[100%] relative" onMouseEnter={() => { setIsHovering(true) }} onMouseLeave={() => { setIsHovering(false) }}>
+      {isHovering && (
+        <Button onClick={()=>{props.onDelete(props.idx)}} variant={"ghost"} className="rounded-xl absolute top-0 right-0" >
+          <TrashIcon />
+        </Button>
+      )}
+      <Card className="h-[100%]">
         <CardHeader>
           <CardTitle>{props.s3Link.title}</CardTitle>
           <CardDescription>{props.s3Link.description}</CardDescription>
         </CardHeader>
         <CardContent>
-          <Button variant={"outline"} className="rounded m-1 w-[100%]">
+          <Button onClick={() => { props.onSyncData(props.idx) }} variant={"outline"} className="rounded m-1 w-[100%]">
             Sync Data
           </Button>
-          <Button variant={"outline"} className="rounded m-1 w-[100%]">
+          <Button onClick={() => { props.onOpenInfo(props.idx) }} variant={"outline"} className="rounded m-1 w-[100%]">
             Open Info
           </Button>
         </CardContent>
