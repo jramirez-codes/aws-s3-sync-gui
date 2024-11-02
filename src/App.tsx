c2a9a7e43b1f6068f855190d4cf150d7e54a8799
Jordan Ramirez <jordan.s.ramirez@gmail.com>
2024-11-02 08:59:05 -0400

    pulling all directories

--- src/App.tsx
+++ src/App.tsx
@@ -5,65 +5,95 @@ import React from "react";
 import { S3Link } from "./types/s3Link";
 import { load } from "@tauri-apps/plugin-store";
 import { InfoSheet } from "./components/infoSheet/infoSheet";
-import { Command } from '@tauri-apps/plugin-shell'
+import { Command } from "@tauri-apps/plugin-shell";
+
 function App() {
-  const [s3Links, setS3Links] = React.useState<S3Link[]>([])
-  const [selectedS3Link, setSelectedS3Link] = React.useState<S3Link | null>(null)
-  const [selectedUploadIdx, setSelectedUploadIdx] = React.useState(-1)
-  const [selectedDownloadIdx, setSelectedDownloadIdx] = React.useState(-1)
+  const [s3Links, setS3Links] = React.useState<S3Link[]>([]);
+  const [selectedS3Link, setSelectedS3Link] = React.useState<S3Link | null>(
+    null,
+  );
+  const [selectedUploadIdx, setSelectedUploadIdx] = React.useState(-1);
+  const [selectedDownloadIdx, setSelectedDownloadIdx] = React.useState(-1);
+  const [isPullingAllDirectories, setIsPullingAllDirectories] =
+    React.useState(false);
 
   async function syncSingleS3Link(idx: number, isUploading: boolean) {
-    let res
+    let res;
     // Handle Upload
     if (isUploading) {
-      setSelectedUploadIdx(_ => idx)
-      res = await Command.create('aws-s3-sync', ['s3', 'sync', s3Links[idx].filePath, s3Links[idx].s3Path]).execute()
-      setSelectedUploadIdx(_ => -1)
+      setSelectedUploadIdx((_) => idx);
+      res = await Command.create("aws-s3-sync", [
+        "s3",
+        "sync",
+        s3Links[idx].filePath,
+        s3Links[idx].s3Path,
+      ]).execute();
+      setSelectedUploadIdx((_) => -1);
     }
     // Handle Download
     else {
-      setSelectedDownloadIdx(_ => idx)
-      res = await Command.create('aws-s3-sync', ['s3', 'sync', s3Links[idx].s3Path, s3Links[idx].filePath]).execute()
-      setSelectedDownloadIdx(_ => -1)
+      setSelectedDownloadIdx((_) => idx);
+      res = await Command.create("aws-s3-sync", [
+        "s3",
+        "sync",
+        s3Links[idx].s3Path,
+        s3Links[idx].filePath,
+      ]).execute();
+      setSelectedDownloadIdx((_) => -1);
+    }
+  }
+
+  async function handlePullAddDirectories() {
+    if (!isPullingAllDirectories) {
+      setIsPullingAllDirectories((_) => true);
+      for (let idx in s3Links) {
+        await syncSingleS3Link(parseInt(idx), false);
+      }
+      setIsPullingAllDirectories((_) => false);
     }
   }
 
   async function handleDeleteS3Link(idx: number) {
     // Remove Link From State
-    let reducedS3Links = s3Links.filter((_, currIdx) => currIdx !== idx)
-    setS3Links(_ => reducedS3Links)
+    let reducedS3Links = s3Links.filter((_, currIdx) => currIdx !== idx);
+    setS3Links((_) => reducedS3Links);
 
     // Update State Store
-    const store = await load('store.json', { autoSave: false });
-    await store.set('s3Links', reducedS3Links)
-    await store.save()
+    const store = await load("store.json", { autoSave: false });
+    await store.set("s3Links", reducedS3Links);
+    await store.save();
 
-    setSelectedS3Link(null)
+    setSelectedS3Link(null);
   }
 
   async function handleOpenInfo(idx: number) {
-    setSelectedS3Link(s3Links[idx])
+    setSelectedS3Link(s3Links[idx]);
   }
-
 
   // Fetch Links
   React.useEffect(() => {
     const asyncFunc = async () => {
-      const store = await load('store.json', { autoSave: false });
-      let newS3Links: S3Link[] = []
+      const store = await load("store.json", { autoSave: false });
+      let newS3Links: S3Link[] = [];
       if (await store.has("s3Links")) {
-        let storeData = await store.get<S3Link[]>('s3Links');
-        newS3Links = storeData ? storeData : []
-        setS3Links(newS3Links)
+        let storeData = await store.get<S3Link[]>("s3Links");
+        newS3Links = storeData ? storeData : [];
+        setS3Links(newS3Links);
       }
-    }
-    asyncFunc()
-  }, [])
+    };
+    asyncFunc();
+  }, []);
 
   return (
     <>
       <div className="h-[100vh] w-[100vw] p-2">
-        <Topbar setS3Links={setS3Links} />
+        <Topbar
+          isPullingAllDirectories={isPullingAllDirectories}
+          setS3Links={setS3Links}
+          onPullAllDirectories={() => {
+            handlePullAddDirectories();
+          }}
+        />
         <div className="grid grid-cols-4 gap-4 items-stretch">
           {s3Links.map((e, idx) => {
             return (
@@ -73,24 +103,29 @@ function App() {
                 key={idx + e.title}
                 isDownloading={selectedDownloadIdx === idx}
                 isUploading={selectedUploadIdx === idx}
-                isFetching={selectedDownloadIdx === idx || selectedUploadIdx === idx}
+                isFetching={
+                  selectedDownloadIdx === idx || selectedUploadIdx === idx
+                }
                 onUpload={(idx: number) => {
-                  syncSingleS3Link(idx, true)
+                  syncSingleS3Link(idx, true);
                 }}
                 onDownload={(idx: number) => {
-                  syncSingleS3Link(idx, false)
+                  syncSingleS3Link(idx, false);
                 }}
                 onDelete={(idx: number) => {
-                  handleDeleteS3Link(idx)
+                  handleDeleteS3Link(idx);
                 }}
                 onOpenInfo={(idx: number) => {
-                  handleOpenInfo(idx)
+                  handleOpenInfo(idx);
                 }}
               />
-            )
+            );
           })}
         </div>
-        <InfoSheet s3Link={selectedS3Link} setSelectedS3Link={setSelectedS3Link} />
+        <InfoSheet
+          s3Link={selectedS3Link}
+          setSelectedS3Link={setSelectedS3Link}
+        />
       </div>
     </>
   );
--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -1,21 +1,36 @@
-import { DownloadIcon, PlusIcon } from "@radix-ui/react-icons";
+import { DownloadIcon, PlusIcon, ReloadIcon } from "@radix-ui/react-icons";
 import { Button } from "../ui/button";
 import { NewDirectoryForm } from "./ui/newDirectoryForm";
 import { S3Link } from "../../types/s3Link";
 import React from "react";
-export function Topbar(props:{setS3Links: React.Dispatch<React.SetStateAction<S3Link[]>>}) {
+export function Topbar(props: {
+  setS3Links: React.Dispatch<React.SetStateAction<S3Link[]>>;
+  onPullAllDirectories: Function;
+  isPullingAllDirectories: boolean;
+}) {
   return (
     <div className="flex justify-between m-2">
       <h1 className="text-xl font-bold mt-1">AWS S3 Sync GUI</h1>
       <div className="flex">
-        <NewDirectoryForm setS3Links={(e)=>props.setS3Links(e)}>
+        <NewDirectoryForm setS3Links={(e) => props.setS3Links(e)}>
           <Button className="rounded m-1" variant={"outline"}>
             <PlusIcon />
             Add New Directory
           </Button>
         </NewDirectoryForm>
-        <Button className="rounded m-1" variant={"outline"}>
-          <DownloadIcon />
+        <Button
+          disabled={props.isPullingAllDirectories}
+          onClick={() => {
+            props.onPullAllDirectories();
+          }}
+          className="rounded m-1"
+          variant={"outline"}
+        >
+          {props.isPullingAllDirectories ? (
+            <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+          ) : (
+            <DownloadIcon />
+          )}
           Pull All Directories
         </Button>
       </div>
